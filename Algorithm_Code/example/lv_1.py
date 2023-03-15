50e704b4c5b88a3481bcaa1754193b50cfb42177
Arvens <lklkk2002@naver.com>
2023-03-16 00:16:45 +0900

    개인정보 수집 유효기간

--- Algorithm_Code/example/lv_1.py
+++ Algorithm_Code/example/lv_1.py
@@ -195,6 +195,30 @@ def solution(today, terms, privacies) :
 
 #     return result
 
+def dateToDay(date):
+    year, month, day = map(int, date.split("."))
+    return (year * 12 * 28) + (month * 28) + day
+    
+def solution(today, terms, privacies):
+    answer = []
+    
+    # today
+    today = dateToDay(today)
+    
+    # terms
+    termsInfo = dict()
+    for term in terms:
+        term = term.split()
+        termsInfo[term[0]] = int(term[1]) * 28
+    
+    # privacies
+    for i in range(len(privacies)):
+        date, term = privacies[i].split()
+        if dateToDay(date) + termsInfo[term] <= today:
+            answer.append(i+1)
+        
+    return answer
+
 if __name__  == '__main__' :
     print(solution('2022.05.19', ["A 6", "B 12", "C 3"], ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]))
     # print(solution("2020.01.01", ["Z 3", "D 5"], ["2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"]))
